738bc151c69d6b6511b2c753fa45605446380a13
Tanush Mahajan <tanush.mohina@gmail.com>
2024-05-28 18:46:44 +0530

    chore: Moved Nav to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const danfofont = localFont({
-	src: "fonts/Danfo-Regular-VariableFont_ELSH.ttf",
-	display: "swap",
-});
-
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
-			<nav className=" w-full h-[60px] px-5 flex justify-start items-center">
-				<div className={`${danfofont.className} text-red-600 text-2xl`}>
-					Tanush&apos;s Portfolio
-				</div>
-			</nav>
-		</main>
-	);
+	return <main className=""></main>;
 }
