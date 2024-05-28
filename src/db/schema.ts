a3cac6419ba5a57b4622e8b64b40dc92e5d7a0cd
Tanush Mahajan <tanush.mohina@gmail.com>
2024-05-28 17:50:20 +0530

    Drizzle configuration

---
+++ src/db/schema.ts
@@ -0,0 +1,48 @@
+import { table } from "console";
+import {
+	integer,
+	pgTable,
+	serial,
+	text,
+	timestamp,
+	primaryKey,
+} from "drizzle-orm/pg-core";
+
+export const projectsTable = pgTable("projects_table", {
+	id: integer("id").primaryKey(),
+	name: text("name").notNull(),
+	url: text("url").notNull(),
+	description: text("description"),
+	// list of tag ids separated by comma
+	// tags: list,
+});
+
+export const tags = pgTable("tags_table", {
+	id: integer("id").primaryKey(),
+	title: text("title").notNull(),
+	// content: text("content").notNull(),
+	// userId: integer("user_id")
+	// 	.notNull()
+	// 	.references(() => usersTable.id, { onDelete: "cascade" }),
+	// createdAt: timestamp("created_at").notNull().defaultNow(),
+	// updatedAt: timestamp("updated_at")
+	// 	.notNull()
+	// 	.$onUpdate(() => new Date()),
+});
+
+export const projectTagsCorrelationTable = pgTable(
+	"project_tags_correlation_table",
+	{
+		projectID: integer("project_id").references(() => projectsTable.id),
+		tagID: integer("tag_id").references(() => tags.id),
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.projectID, table.tagID] }),
+	})
+);
+
+export type InsertUser = typeof projectsTable.$inferInsert;
+export type SelectUser = typeof projectsTable.$inferSelect;
+
+export type InsertPost = typeof tags.$inferInsert;
+export type SelectPost = typeof tags.$inferSelect;
